67ee2f759df9c34f312ae5688c436741d077ba3d
OLoKo64 <naldoepingo1@gmail.com>
2022-10-22 21:58:39 -0300

    feat: add initial files

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use actix_web::{web, App, HttpServer};
+mod lib;
+mod paths;
+
+const PORT: u16 = 8080;
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    println!("Starting server at http://localhost:{}", PORT);
+    println!("API Version: {}", env!("CARGO_PKG_VERSION"));
+    HttpServer::new(|| {
+        App::new()
+            .route(
+                "/",
+                web::get().to(|| async { "CPF Generator" }),
+            )
+            .service(paths::new_cpf)
+            .service(paths::new_cpf_state_code)
+            .service(paths::validate_cpf)
+    })
+    .bind(("127.0.0.1", PORT))?
+    .run()
+    .await
+}
---
+++ src/paths.rs
@@ -0,0 +1,90 @@
+use actix_web::{error, get, web, Responder};
+use derive_more::{Display, Error};
+use serde::{Deserialize, Serialize};
+
+use crate::lib;
+
+#[derive(Debug, Display, Error)]
+#[display(fmt = "error: {}", message)]
+pub struct ResponseError {
+    message: &'static str,
+}
+
+// Use default implementation for `error_response()` method
+impl error::ResponseError for ResponseError {}
+
+#[derive(Deserialize)]
+pub struct Info {
+    qtd: Option<String>,
+}
+
+#[derive(Debug, Serialize)]
+struct ValidateResponse {
+    is_valid: bool,
+    error: Option<String>,
+}
+
+#[get("/new-cpf")]
+pub async fn new_cpf(qtd: web::Query<Info>) -> impl Responder {
+    let qtd = qtd
+        .qtd
+        .clone()
+        .unwrap_or_else(|| "1".to_string())
+        .parse::<u32>()
+        .unwrap_or(1);
+    let mut cpfs = Vec::new();
+    for _ in 0..qtd {
+        cpfs.push(lib::generate_cpf(None, None));
+    }
+    web::Json(cpfs)
+}
+
+#[get("/new-cpf/{state_code}")]
+pub async fn new_cpf_state_code(
+    qtd: web::Query<Info>,
+    state_code: web::Path<String>,
+) -> Result<impl Responder, ResponseError> {
+    let state_code_parsed: u8 = match state_code.parse::<u8>() {
+        Ok(state_code) => {
+            if state_code > 9 {
+                return Err(ResponseError {
+                    message: "Invalid state code. Must be a number between 0 and 9.",
+                });
+            }
+            state_code
+        }
+        Err(_) => {
+            return Err(ResponseError {
+                message: "Invalid state code. Must be a number between 0 and 9.",
+            })
+        }
+    };
+    let qtd = qtd
+        .qtd
+        .clone()
+        .unwrap_or_else(|| "1".to_string())
+        .parse::<u32>()
+        .unwrap_or(1);
+    let mut cpfs = Vec::new();
+    for _ in 0..qtd {
+        cpfs.push(lib::generate_cpf(Some(state_code_parsed), None));
+    }
+    Ok(web::Json(cpfs))
+}
+
+#[get("/validate-cpf/{cpf}")]
+pub async fn validate_cpf(cpf: web::Path<String>) -> impl Responder {
+    let cpf = cpf.to_string();
+    let is_valid = lib::validate_cpf(&cpf);
+    if let Err(error_message) = is_valid {
+        web::Json(ValidateResponse {
+            is_valid: false,
+            error: Some(error_message.to_string()),
+        })
+    } else {
+        web::Json(ValidateResponse {
+            is_valid: true,
+            error: None,
+        })
+    }
+}
