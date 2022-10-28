9a101a4f2cc5555dcffa85be467200e2fdaac731
OLoKo64 <naldoepingo1@gmail.com>
2022-10-28 14:56:41 -0300

    feat: allow any cors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{web, App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{http::header, web, App, HttpServer};
 mod paths;
 mod types;
 mod utils;
@@ -10,7 +11,10 @@ async fn main() -> std::io::Result<()> {
     println!("Starting server at http://localhost:{}", PORT);
     println!("API Version: {}", env!("CARGO_PKG_VERSION"));
     HttpServer::new(|| {
+        let cors = Cors::default().allow_any_origin().allowed_methods(vec!["GET"]).allowed_header(header::CONTENT_TYPE)
+        .max_age(3600);
         App::new()
+        .wrap(cors)
             .route("/", web::get().to(|| async { "CPF Generator and Validator API. For more information see https://github.com/OLoKo64/actix-cpf-generator-api" }))
             .service(paths::new_cpf)
             .service(paths::validate_cpf)
